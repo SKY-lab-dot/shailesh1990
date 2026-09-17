b653def96b40de3b31885cdf431f1d46b5eb8a9d
SKY-lab-dot <shlshyadav4@gmail.com>
2026-09-17 12:42:30 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,13 +1,17 @@
 terraform {
-    required_providers {
-        source="hashcorp/azurerm"
-        version="4.79.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.79.0"
     }
-  
+
+  }
+
 }
 provider "azurerm" {
-    features {
-      
-    }
-  
+  features {
+
+  }
+  subscription_id = "5ef4ee5d-6bd4-4c27-acc2-af8816fed413"
+
 }
